e1dd788e83338e7ff48493a69098ffdff185442d
ANJALI PRIYA <64773813+AnjaliPriyaa@users.noreply.github.com>
2023-04-15 01:58:50 +0530

    changes push

--- src/components/Network.js
+++ src/components/Network.js
@@ -1,5 +1,5 @@
-import { Typography } from '@mui/material';
 import React from 'react';
+import { Typography } from '@mui/material';
 var container;
 var cn;
 var sources;
@@ -8,7 +8,7 @@ class Network extends React.Component{
   text=-1;
   constructor(props){
     super(props);
-    this.state={apiResponse:[]};
+    this.state={apiResponse:[],apiNetwork:[]};
   }
  
   Network(){
@@ -16,24 +16,25 @@ class Network extends React.Component{
     .then(res=>res.text())
     .then(res=>{this.setState({apiResponse:JSON.parse(res)});this.val=2});//.then(res=>this.setState({apiResponse1:res.replace(/\\t/gi, '   ')}));
   }
-  // Neetwork(){
-  //   fetch(`http://localhost:9000/Network-F A L S E`)
-  //   .then(res=>res.text())
-  //   .then(res=>{this.setState({apiResponse:JSON.parse(res)});this.val=3;});//.then(res=>this.setState({apiResponse1:res.replace(/\\t/gi, '   ')}));
-  // }
   Neet=(event, source) =>{
-    fetch(`http://localhost:9000/Network-${this.state.apiResponse[source]}`)
-    .then(res=>res.text())
-    .then(res=>{this.setState({apiResponse:JSON.parse(res)});
-    sources=source;
-    container=JSON.parse(JSON.parse(res))[0];
-    cn=container["Name"];
-    container=container["Containers"];
-    var count=[];
-    for (let key in container)count.push(container[key]["Name"]);
-    container=count;
+   
+   fetch(`http://localhost:9000/Network-${this.state.apiResponse[source]}`)
+   .then(res=>res.text())
+   .then(res=>{this.setState({apiNetwork:JSON.parse(res)});
+   sources=source;
+   container=JSON.parse(this.state.apiNetwork)[0];
+   cn=container["Name"];
+   container=container["Containers"];
+   var count=[];
+   for (let key in container)count.push(container[key]["Name"]);
+   container=count;
+   this.val=3;
+});
+  }
+  Download(){
+    fetch(`http://localhost:9000/download-${cn}`)
+    .then(res=>{alert("done"+cn)})
     
-    this.val=3;});//.then(res=>this.setState({apiResponse1:res.replace(/\\t/gi, '   ')}));
   }
   soln(){
     fetch(`http://localhost:9000/Network-${cn}`)
@@ -44,57 +45,43 @@ class Network extends React.Component{
     for (let key in container)count.push(container[key]["Name"]);
     container=count;
     this.val=3;});
-    document.getElementsByClassName('Nbody2')[0].style.display="none";
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    ctx.fillStyle = "#1976d2";
-    ctx.fillRect(0,0,740,569);
-    ctx.beginPath();
-    ctx.arc(390, 284, 50, 0, 2 * Math.PI);
-    ctx.font = "15px Verdana";
-    // Create gradient
-    var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
-    gradient.addColorStop("0"," magenta");
-    gradient.addColorStop("0.5", "blue");
-    gradient.addColorStop("1.0", "red");
-    ctx.fillStyle = gradient;
-    ctx.fillText(cn,341,290);
-    var x=container.length,i=0;
-    var angle=360/x;
-    for(i=0;i<x;i++)
-    {
-    ctx.moveTo(390+50*Math.cos(i*angle*3.14/180),284-50*Math.sin(i*angle*3.14/180));
-    ctx.lineTo(390+160*Math.cos(i*angle*3.14/180),284-160*Math.sin(i*angle*3.14/180));
+    ctx.fillStyle = "#B0DAFF";
+ctx.fillRect(0,0,1000,569);
+ctx.beginPath();
+ctx.arc(390, 284, 50, 0, 2 * Math.PI);
+ctx.font = "15px Verdana";
+// Create gradient
+var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+gradient.addColorStop("0"," magenta");
+gradient.addColorStop("0.5", "blue");
+gradient.addColorStop("1.0", "red");
+ctx.fillStyle = gradient;
+ctx.fillText(cn,341,290);
+var x=container.length,i=0;
+var angle=360/x;
+for(i=0;i<x;i++)
+{
+ctx.moveTo(390+50*Math.cos(i*angle*3.14/180),284-50*Math.sin(i*angle*3.14/180));
+ctx.lineTo(390+160*Math.cos(i*angle*3.14/180),284-160*Math.sin(i*angle*3.14/180));
 
-    }
-    ctx.stroke();
-    for(i=0;i<x;i++)
-    {
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.font = "10px Verdana";
-    ctx.fillText(container[i],352+200*Math.cos(i*angle*3.14/180),284-200*Math.sin(i*angle*3.14/180));
-    ctx.arc(390+200*Math.cos(i*angle*3.14/180), 284-200*Math.sin(i*angle*3.14/180), 40, 0, 2 * Math.PI);
-    ctx.stroke();
-    }
-    document.getElementsByClassName('Nbody3')[0].style.display="inline-block";
+}
+ctx.stroke();
+for(i=0;i<x;i++)
+{
+ctx.beginPath();
+ctx.fillStyle = "red";
+ctx.font = "10px Verdana";
+ctx.fillText(container[i],352+200*Math.cos(i*angle*3.14/180),284-200*Math.sin(i*angle*3.14/180));
+ctx.arc(390+200*Math.cos(i*angle*3.14/180), 284-200*Math.sin(i*angle*3.14/180), 40, 0, 2 * Math.PI);
+ctx.stroke();
+}
   }
-  solv(){
-    fetch(`http://localhost:9000/Network-${cn}`)
-    .then(res=>res.text())
-    .then(res=>{this.setState({apiResponse:JSON.parse(res)});
-    container=JSON.parse(JSON.parse(res))[0]["Containers"];
-    var count=[];
-    for (let key in container)count.push(container[key]["Name"]);
-    container=count;
-    this.val=3;});
-    document.getElementsByClassName('Nbody2')[0].style.display="inline-block";
-    document.getElementsByClassName('Nbody3')[0].style.display="none";
-  }
+  
   componentWillMount(){
     
   }
-  
   
 render(){
     
@@ -121,15 +108,15 @@ for (let i=0;i<this.state.apiResponse.length-1; ++i) {
   
 }
 else if(this.val===3){
-    return (
-      <div className="Network">
-   
-   
-    <pre className="Nbody2"><button className='chngbutton'  onClick={event =>{this.soln();}}>getnames</button>{this.state.apiResponse}</pre>
-    <pre className="Nbody3"><button className='chngbutton'  onClick={event =>{this.solv();}}>getnames</button><canvas id="myCanvas" width="740" height="569">.</canvas>
-    </pre>
-   </div>
-    );
+  
+  return (
+    <div className="Network" onMouseEnter={event =>{this.soln();}}>
+ 
+ 
+  <pre className="Nbody2"><button className='chngbutton'  onClick={event =>{this.Download();}}>download</button><canvas id="myCanvas" width="1000" height="569">.</canvas></pre>
+  
+ </div>
+  );
 }
 }
 
